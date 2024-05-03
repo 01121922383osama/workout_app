41e8b0a6f7c7e922040f7fe8daaf8c28b703e736
siraj <sirajhouloubi1@gmail.com>
2024-05-03 18:28:54 +0300

    button to add a exercise

--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -5,7 +5,8 @@ import 'package:workout_app/data/work_out_data.dart';
 
 class Workoutpage extends StatefulWidget {
   final String workoutName;
-  const Workoutpage({super.key, required this.workoutName});
+
+  Workoutpage({super.key, required this.workoutName});
 
   @override
   State<Workoutpage> createState() => _WorkoutpageState();
@@ -18,11 +19,111 @@ class _WorkoutpageState extends State<Workoutpage> {
         .checkOffExercies(workoutName, exerciesName);
   }
 
+  //text controller
+  final exerciseNameController = TextEditingController();
+
+  // wieght controller
+  final wieghtController = TextEditingController();
+  // reps controller
+  final repsController = TextEditingController();
+  // sets controller
+  final setsController = TextEditingController();
+
+  //create a new exercise
+  void createNewExercise() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Add a new exercise'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //exercise name
+                  TextField(
+                    controller: exerciseNameController,
+                    decoration:
+                        const InputDecoration(hintText: 'Exercise Name '),
+                  ),
+                  //wieght
+                  TextField(
+                    controller: wieghtController,
+                    decoration: const InputDecoration(hintText: ' Wieght '),
+                  ),
+                  // reps
+                  TextField(
+                    controller: setsController,
+                    decoration: const InputDecoration(hintText: ' Reps'),
+                  ),
+                  //sets
+                  TextField(
+                    controller: repsController,
+                    decoration: const InputDecoration(hintText: ' Sets'),
+                  )
+                ],
+              ),
+              actions: [
+                // save button
+                MaterialButton(
+                  onPressed: save,
+                  child: Text('save'),
+                ),
+
+                // cancel button
+                MaterialButton(
+                  onPressed: cansel,
+                  child: Text('cancel'),
+                )
+              ],
+            ));
+  }
+
+  //save workout
+  void save() {
+    //get exercise name for the text controller
+    String newExerciseName = exerciseNameController.text;
+    String wieght = wieghtController.text;
+    String sets = setsController.text;
+    String reps = repsController.text;
+
+    // add exercise to the workout list
+    Provider.of<WorkoutData>(context, listen: false).addExercies(
+      widget.workoutName,
+      newExerciseName,
+      wieght,
+      reps,
+      sets,
+    );
+    // to pop out after save
+    Navigator.pop(context);
+    // to clear the text filed
+    clear();
+  }
+
+  //cansel
+  void cansel() {
+    //to pop out after cansel
+    Navigator.pop(context);
+    // to clear the text filed
+    clear();
+  }
+
+  // clear controler
+  void clear() {
+    exerciseNameController.clear();
+    setsController.clear();
+    repsController.clear();
+    wieghtController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
       builder: (context, value, child) => Scaffold(
         appBar: AppBar(title: Text(widget.workoutName)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: createNewExercise,
+          child: const Icon(Icons.add),
+        ),
         body: ListView.builder(
             itemCount: value.theNumberOfExerciseInWorkout(widget.workoutName),
             itemBuilder: (context, index) => ExerciseTile(
@@ -54,7 +155,6 @@ class _WorkoutpageState extends State<Workoutpage> {
                         .exercise[index]
                         .name,
                   );
-                  print(value);
                 })),
       ),
     );
